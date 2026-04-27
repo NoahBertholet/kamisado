db8e290a6437f9f0a7b71e8e2c3f124e2433675b
NoahBertholet <bertholet.noah.04@gmail.com>
2026-04-27 13:13:19 +0200

    test pour handle_message ping et giveup

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from bot import send_json, receive_json
+from bot import send_json, receive_json, handle_message
 
 
 def test_send_and_receive_json():
@@ -20,3 +20,30 @@ def test_send_and_receive_json():
     socket_2.close()
 
     assert received == message
+def test_handle_message_ping():
+    socket_1, socket_2 = socket.socketpair()
+
+    send_json(socket_1, {"request": "ping"})
+
+    handle_message(socket_2)
+
+    response = receive_json(socket_1)
+
+    socket_1.close()
+    socket_2.close()
+
+    assert response == {"response": "pong"}
+
+def test_handle_message_giveup():
+    socket_1, socket_2 = socket.socketpair()
+
+    send_json(socket_1, {"request": "play"})
+
+    handle_message(socket_2)
+
+    response = receive_json(socket_1)
+
+    socket_1.close()
+    socket_2.close()
+
+    assert response == {"response": "giveup"}
