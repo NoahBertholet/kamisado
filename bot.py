17eb1f4ce75b63e42cebb8d78dd5d32ce5c19fb7
NoahBertholet <bertholet.noah.04@gmail.com>
2026-05-01 11:51:26 +0200

    nom tournoi

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import time
 
 # constantes
 BOT_PORT = 8888
-BOT_NAME = "BERTHOFUSEE"
+BOT_NAME = "ancienne"
 SERVER_HOST = "localhost"
 SERVER_PORT = 3000
 
@@ -432,7 +432,7 @@ def build_subscribe_message():
         "request": "subscribe",
         "port": BOT_PORT,
         "name": BOT_NAME,
-        "matricules": ["24371", "23032"]
+        "matricules": ["17866", "66178"]
     }
 
 def subscribe_to_server():
